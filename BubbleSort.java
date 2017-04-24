16309a242f9aa52e812b1266f8b7e6edebaae110
Brownie456 <kbrown23@albany.edu>
2017-04-24 07:32:18 -0400

    commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,4 +2,54 @@
  * Created by Brownie on 4/16/2017.
  */
 public class BubbleSort {
+    private static Node head;
+    private static Node tail;
+    private int size;
+
+    public BubbleSort() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void forwardTrav() {
+        Node temp = head;
+        System.out.print("Iterate Forward:\n");
+        if (temp == null) {
+            System.out.print("List is empty");
+        }
+        while (temp != null) {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+    }
+
+    public void add(String data) {
+        Node newNode = new Node(data, null, null);
+        Node temp, holder;
+        //if the list is empty put item into the list
+        if (head == null) {
+            head = newNode;
+            tail = head;
+        } else {
+            temp = head;
+            holder = head.getNext();
+            //if the data is lower alphabetically do this
+            while (holder != null) {
+                //if data has reached end of the list then put data here
+                if (holder.getNext() == null) {
+                    holder.setNext(newNode);
+                    newNode.setPrev(holder);
+                    tail = newNode;
+                    break;
+                }
+                //iterates through the list
+                else {
+                    temp = holder;
+                    holder = holder.getNext();
+                }
+            }
+        }
+        size++;
+    }
 }
--- Node.java
+++ Node.java
@@ -2,4 +2,59 @@
  * Created by Brownie on 4/16/2017.
  */
 public class Node {
+    private String data;
+    private Node next, prev;
+    /*Constructor method */
+    public Node(){
+        data = "";
+        next = null;
+        prev = null;
+
+    }
+    /*  Constructor with parameters*/
+    public Node(String data,Node next, Node prev) {
+
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    /* Get next node method*/
+    public Node getNext() {
+        return next;
+    }
+
+    /* Set next node method*/
+    public void setNext(Node next)
+    {
+        this.next = next;
+    }
+
+    /* Method to return previous node */
+    public Node getPrev(){
+        return prev;
+    }
+
+    /*Method to set previous node */
+    public void setPrev(Node prev){
+        this.prev = prev;
+    }
+
+    /*method to return the node in String */
+    public String getData() {
+        return data;
+    }
+
+    /*Method to set the data of node */
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    /*To string method to return the data of node */
+    public String toString() {
+        return " Name : "+this.data;
+    }
+
 }
+
+
